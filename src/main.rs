05912ca9f9fa5c3e259ae5b479ab00940c671042
Elliot Smith <elliot@elliotcsmith.com>
2024-12-17 19:59:47 +1000

    Day 17 Part 2

--- src/main.rs
+++ src/main.rs
@@ -49,91 +49,118 @@ pub fn day17(filename: String, part_b: bool) {
             3 => {}
             4 => {
                 instructions = right.split(',').map(|x| x.parse().unwrap()).collect();
-                target = right.trim().to_string();
             }
             _ => {}
         }
     }
     debug!(a=register_a, b=register_b, c=register_c, instructions=?instructions, "Read Program");
-    let mut out_pointer = 0;
+
+    let mut out_pointer;
+    let mut a_test = 0;
+
+    if part_b {
+        a_test = 0; //8u64.pow(instructions.len() as u32);
+    }
 
     loop {
-        if instruction_pointer >= instructions.len() {
+        register_a = (a_test << 24) + 0b11101001011011010010011100000;
+        register_b = 0;
+        register_c = 0;
+        instruction_pointer = 0;
+
+        out_pointer = 0;
+        loop {
+            if instruction_pointer >= instructions.len() {
+                debug!("End of Sequence");
+                break;
+            }
+            let op_code = instructions[instruction_pointer];
+            let operand = instructions[instruction_pointer + 1];
+
+            let operand_value = match operand {
+                0 | 1 | 2 | 3 => operand as u64,
+                4 => register_a,
+                5 => register_b,
+                6 => register_c,
+                7 => 0,
+                _ => 0,
+            };
+
+            match op_code {
+                0 => {
+                    // adv
+                    let div = 2u64.pow(operand_value as u32) as u64;
+                    let result = register_a / div;
+                    register_a = result;
+                }
+                1 => {
+                    // bxl
+                    let result = register_b ^ operand as u64;
+                    register_b = result;
+                }
+                2 => {
+                    // bst
+                    let result = operand_value % 8;
+                    register_b = result;
+                }
+                3 => {
+                    // jnz
+                    if register_a != 0 {
+                        instruction_pointer = operand as usize;
+                        continue;
+                    }
+                }
+                4 => {
+                    // bxc
+                    register_b = register_b ^ register_c;
+                }
+                5 => {
+                    // out
+                    let result = operand_value % 8;
+                    if result as u8 != instructions[out_pointer as usize] {
+                        if out_pointer > 8 {
+                            debug!(
+                                a = a_test,
+                                out = out_pointer,
+                                "Bailed on output mismatch {:b}",
+                                a_test
+                            );
+                        }
+                        break;
+                    }
+
+                    out_pointer += 1;
+
+                    if out_pointer >= instructions.len() {
+                        debug!("Bailed on output Length");
+                    }
+                }
+                6 => {
+                    // bdv
+                    let div = 2u64.pow(operand_value as u32) as u64;
+                    let result = register_a / div;
+                    register_b = result;
+                }
+                7 => {
+                    // cdv
+                    let div = 2u64.pow(operand_value as u32) as u64;
+                    let result = register_a / div;
+                    register_c = result;
+                }
+                _ => {
+                    error!(op_code = op_code, "Invalid Op Code");
+                }
+            };
+            instruction_pointer += 2;
+        }
+        if out_pointer == instructions.len() {
+            register_a = (a_test << 24) + 0b11101001011011010010011100000;
+            info!(a_test = register_a, "Finished Run");
             break;
         }
-        let op_code = instructions[instruction_pointer];
-        let operand = instructions[instruction_pointer + 1];
 
-        let operand_value = match operand {
-            0 | 1 | 2 | 3 => operand as u64,
-            4 => register_a,
-            5 => register_b,
-            6 => register_c,
-            7 => 0,
-            _ => 0,
-        };
-
-        debug!(
-            a = register_a,
-            b = register_b,
-            c = register_c,
-            pt = instruction_pointer,
-            "Step"
-        );
-
-        match op_code {
-            0 => {
-                // adv
-                let div = 2u64.pow(operand_value as u32) as u64;
-                let result = register_a / div;
-                debug!(val = operand_value, div = div, result = result, "ADV");
-                register_a = result;
-            }
-            1 => {
-                // bxl
-                let result = register_b ^ operand as u64;
-                register_b = result;
-            }
-            2 => {
-                // bst
-                let result = operand_value % 8;
-                register_b = result;
-            }
-            3 => {
-                // jnz
-                if register_a != 0 {
-                    instruction_pointer = operand as usize;
-                    continue;
-                }
-            }
-            4 => {
-                // bxc
-                register_b = register_b ^ register_c;
-            }
-            5 => {
-                // out
-                debug!(val = operand_value, result = operand_value % 8, "OUT");
-                print!("{:?},", operand_value % 8);
-            }
-            6 => {
-                // bdv
-                let div = 2u64.pow(operand_value as u32) as u64;
-                let result = register_a / div;
-                register_b = result;
-            }
-            7 => {
-                // cdv
-                let div = 2u64.pow(operand_value as u32) as u64;
-                let result = register_a / div;
-                register_c = result;
-            }
-            _ => {
-                error!(op_code = op_code, "Invalid Op Code");
-            }
-        };
-        instruction_pointer += 2;
+        a_test += 1;
     }
-    print!("\n");
 }
 
 fn main() {
@@ -142,6 +169,6 @@ fn main() {
         .init();
     info!("Tracing Setup");
 
-    day17("./inputs/day17small.txt".to_string(), true);
+    //day17("./inputs/day17bsmall.txt".to_string(), true);
     day17("./inputs/day17.txt".to_string(), true);
 }
