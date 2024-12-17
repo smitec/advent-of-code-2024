a63c34746621c1dea9fad92186d9d52fdc4753ac
Elliot Smith <elliot@elliotcsmith.com>
2024-12-17 17:44:32 +1000

    Day 17 Part 1

--- src/done.rs
+++ src/done.rs
@@ -11,6 +11,120 @@ use std::{
 use regex::Regex;
 use tracing::{Level, debug, error, event, info, instrument, warn};
 
+fn display_map(walls: &HashSet<(i32, i32)>, spots: &HashSet<(i32, i32)>, rows: i32, cols: i32) {
+    for row in 0..rows {
+        for col in 0..cols {
+            if walls.contains(&(row, col)) {
+                print!("#");
+            } else if spots.contains(&(row, col)) {
+                print!("O");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+}
+
+#[instrument]
+pub fn day16(filename: String, part_b: bool) {
+    let content = fs::read_to_string(filename).expect("Couldn't read input");
+
+    let mut walls: HashSet<(i32, i32)> = HashSet::new();
+    let mut start: (i32, i32) = (0, 0);
+    let mut end: (i32, i32) = (0, 0);
+
+    let mut rows: i32 = 0;
+    let mut cols: i32 = 0;
+
+    for (row, line) in content.lines().enumerate() {
+        rows += 1;
+        cols = line.len() as i32;
+        for (col, c) in line.chars().enumerate() {
+            match c {
+                '#' => {
+                    walls.insert((row as i32, col as i32));
+                }
+                'S' => {
+                    start = (row as i32, col as i32);
+                }
+                'E' => {
+                    end = (row as i32, col as i32);
+                }
+                _ => {}
+            };
+        }
+    }
+
+    let mut scores: HashMap<(i32, i32), i32> = HashMap::new();
+    let mut spots: HashSet<(i32, i32)> = HashSet::new();
+    let mut front: Vec<((i32, i32), Direction, i32, HashSet<(i32, i32)>)> = Vec::new();
+
+    scores.insert(start, 0);
+    let mut start_spots: HashSet<(i32, i32)> = HashSet::new();
+    start_spots.insert(start);
+    front.push((start, Direction::East, 0, start_spots));
+
+    // TODO: There are multiple ways to step and turn with different scores at different points
+    // Storing only the absolute minimum is not going to cut it. Maybe need to be more exhaustive.
+
+    while let Some(next) = front.pop() {
+        // Straight
+        let next_spot = move_direction(&next.0, &next.1);
+        if !walls.contains(&next_spot) {
+            let current_score: i32 = *scores.get(&next_spot).unwrap_or(&(next.2 + 2));
+            if next.2 + 1 <= current_score {
+                scores.insert(next_spot, next.2 + 1);
+
+                let mut new_spots = next.3.clone();
+                new_spots.insert(next_spot);
+
+                if next_spot == end {
+                    if next.2 + 1 == current_score {
+                        spots.extend(&new_spots);
+                    } else {
+                        spots = new_spots.clone();
+                    }
+                }
+
+                front.insert(0, (next_spot, next.1.clone(), next.2 + 1, new_spots));
+            }
+        }
+
+        // Left & Right
+        for turning in [Rotation::Left, Rotation::Right] {
+            let test_direction = turn(&next.1, turning);
+            let next_spot = move_direction(&next.0, &test_direction);
+            if !walls.contains(&next_spot) {
+                let current_score: i32 = *scores.get(&next_spot).unwrap_or(&(next.2 + 1002));
+                if next.2 + 1001 <= current_score {
+                    scores.insert(next_spot, next.2 + 1001);
+
+                    let mut new_spots = next.3.clone();
+                    new_spots.insert(next_spot);
+
+                    if next_spot == end {
+                        if next.2 + 1001 == current_score {
+                            spots.extend(&new_spots);
+                        } else {
+                            spots = new_spots.clone();
+                        }
+                    }
+
+                    // front.insert(0, (next_spot, test_direction, next.2 + 1001, new_spots));
+                    front.push((next_spot, test_direction, next.2 + 1001, new_spots));
+                }
+            }
+        }
+
+        //front.sort_by_key(|x| x.2);
+    }
+
+    display_map(&walls, &spots, rows, cols);
+
+    info!(score=?scores.get(&end), spots=?spots.len(), "Finished Pathfinding");
+}
+
 #[derive(Debug, Clone, Copy)]
 enum Tile {
     Wall,
@@ -20,7 +134,7 @@ enum Tile {
     BoxRight,
 }
 
-fn display_map(map: &HashMap<(i32, i32), Tile>, rows: i32, cols: i32, player: (i32, i32)) {
+fn display_map_15(map: &HashMap<(i32, i32), Tile>, rows: i32, cols: i32, player: (i32, i32)) {
     for row in 0..rows {
         for col in 0..cols {
             if player.0 == row && player.1 == col {
@@ -303,7 +417,7 @@ pub fn day15(filename: String, part_b: bool) {
         }
     }
 
-    display_map(&map, rows, cols * 2, player);
+    display_map_15(&map, rows, cols * 2, player);
 
     let mut total = 0;
     for ((r, c), value) in map.iter() {
--- src/main.rs
+++ src/main.rs
@@ -17,118 +17,123 @@ pub fn dayxx(filename: String, part_b: bool) {
     let content = fs::read_to_string(filename).expect("Couldn't read input");
 }
 
-fn display_map(walls: &HashSet<(i32, i32)>, spots: &HashSet<(i32, i32)>, rows: i32, cols: i32) {
-    for row in 0..rows {
-        for col in 0..cols {
-            if walls.contains(&(row, col)) {
-                print!("#");
-            } else if spots.contains(&(row, col)) {
-                print!("O");
-            } else {
-                print!(".");
-            }
-        }
-        print!("\n");
-    }
-}
-
 #[instrument]
-pub fn day16(filename: String, part_b: bool) {
+pub fn day17(filename: String, part_b: bool) {
     let content = fs::read_to_string(filename).expect("Couldn't read input");
 
-    let mut walls: HashSet<(i32, i32)> = HashSet::new();
-    let mut start: (i32, i32) = (0, 0);
-    let mut end: (i32, i32) = (0, 0);
+    let mut register_a: u64 = 0;
+    let mut register_b: u64 = 0;
+    let mut register_c: u64 = 0;
+    let mut instruction_pointer: usize = 0;
+    let mut instructions: Vec<u8> = Vec::new();
 
-    let mut rows: i32 = 0;
-    let mut cols: i32 = 0;
+    let mut target: String = "".to_string();
 
-    for (row, line) in content.lines().enumerate() {
-        rows += 1;
-        cols = line.len() as i32;
-        for (col, c) in line.chars().enumerate() {
-            match c {
-                '#' => {
-                    walls.insert((row as i32, col as i32));
-                }
-                'S' => {
-                    start = (row as i32, col as i32);
-                }
-                'E' => {
-                    end = (row as i32, col as i32);
-                }
-                _ => {}
-            };
+    for (i, line) in content.lines().enumerate() {
+        if line.len() == 0 {
+            continue;
+        }
+
+        let (_, right) = line.split_once(": ").unwrap();
+
+        match i {
+            0 => {
+                register_a = right.parse().unwrap();
+            }
+            1 => {
+                register_b = right.parse().unwrap();
+            }
+            2 => {
+                register_c = right.parse().unwrap();
+            }
+            3 => {}
+            4 => {
+                instructions = right.split(',').map(|x| x.parse().unwrap()).collect();
+                target = right.trim().to_string();
+            }
+            _ => {}
         }
     }
+    debug!(a=register_a, b=register_b, c=register_c, instructions=?instructions, "Read Program");
+    let mut out_pointer = 0;
 
-    let mut scores: HashMap<(i32, i32), i32> = HashMap::new();
-    let mut spots: HashSet<(i32, i32)> = HashSet::new();
-    let mut front: Vec<((i32, i32), Direction, i32, HashSet<(i32, i32)>)> = Vec::new();
-
-    scores.insert(start, 0);
-    let mut start_spots: HashSet<(i32, i32)> = HashSet::new();
-    start_spots.insert(start);
-    front.push((start, Direction::East, 0, start_spots));
-
-    // TODO: There are multiple ways to step and turn with different scores at different points
-    // Storing only the absolute minimum is not going to cut it. Maybe need to be more exhaustive.
-
-    while let Some(next) = front.pop() {
-        // Straight
-        let next_spot = move_direction(&next.0, &next.1);
-        if !walls.contains(&next_spot) {
-            let current_score: i32 = *scores.get(&next_spot).unwrap_or(&(next.2 + 2));
-            if next.2 + 1 <= current_score {
-                scores.insert(next_spot, next.2 + 1);
-
-                let mut new_spots = next.3.clone();
-                new_spots.insert(next_spot);
-
-                if next_spot == end {
-                    if next.2 + 1 == current_score {
-                        spots.extend(&new_spots);
-                    } else {
-                        spots = new_spots.clone();
-                    }
-                }
-
-                front.insert(0, (next_spot, next.1.clone(), next.2 + 1, new_spots));
-            }
+    loop {
+        if instruction_pointer >= instructions.len() {
+            break;
         }
+        let op_code = instructions[instruction_pointer];
+        let operand = instructions[instruction_pointer + 1];
 
-        // Left & Right
-        for turning in [Rotation::Left, Rotation::Right] {
-            let test_direction = turn(&next.1, turning);
-            let next_spot = move_direction(&next.0, &test_direction);
-            if !walls.contains(&next_spot) {
-                let current_score: i32 = *scores.get(&next_spot).unwrap_or(&(next.2 + 1002));
-                if next.2 + 1001 <= current_score {
-                    scores.insert(next_spot, next.2 + 1001);
+        let operand_value = match operand {
+            0 | 1 | 2 | 3 => operand as u64,
+            4 => register_a,
+            5 => register_b,
+            6 => register_c,
+            7 => 0,
+            _ => 0,
+        };
 
-                    let mut new_spots = next.3.clone();
-                    new_spots.insert(next_spot);
+        debug!(
+            a = register_a,
+            b = register_b,
+            c = register_c,
+            pt = instruction_pointer,
+            "Step"
+        );
 
-                    if next_spot == end {
-                        if next.2 + 1001 == current_score {
-                            spots.extend(&new_spots);
-                        } else {
-                            spots = new_spots.clone();
-                        }
-                    }
-
-                    // front.insert(0, (next_spot, test_direction, next.2 + 1001, new_spots));
-                    front.push((next_spot, test_direction, next.2 + 1001, new_spots));
+        match op_code {
+            0 => {
+                // adv
+                let div = 2u64.pow(operand_value as u32) as u64;
+                let result = register_a / div;
+                debug!(val = operand_value, div = div, result = result, "ADV");
+                register_a = result;
+            }
+            1 => {
+                // bxl
+                let result = register_b ^ operand as u64;
+                register_b = result;
+            }
+            2 => {
+                // bst
+                let result = operand_value % 8;
+                register_b = result;
+            }
+            3 => {
+                // jnz
+                if register_a != 0 {
+                    instruction_pointer = operand as usize;
+                    continue;
                 }
             }
-        }
-
-        //front.sort_by_key(|x| x.2);
+            4 => {
+                // bxc
+                register_b = register_b ^ register_c;
+            }
+            5 => {
+                // out
+                debug!(val = operand_value, result = operand_value % 8, "OUT");
+                print!("{:?},", operand_value % 8);
+            }
+            6 => {
+                // bdv
+                let div = 2u64.pow(operand_value as u32) as u64;
+                let result = register_a / div;
+                register_b = result;
+            }
+            7 => {
+                // cdv
+                let div = 2u64.pow(operand_value as u32) as u64;
+                let result = register_a / div;
+                register_c = result;
+            }
+            _ => {
+                error!(op_code = op_code, "Invalid Op Code");
+            }
+        };
+        instruction_pointer += 2;
     }
-
-    display_map(&walls, &spots, rows, cols);
-
-    info!(score=?scores.get(&end), spots=?spots.len(), "Finished Pathfinding");
+    print!("\n");
 }
 
 fn main() {
@@ -137,6 +142,6 @@ fn main() {
         .init();
     info!("Tracing Setup");
 
-    day16("./inputs/day16small.txt".to_string(), true);
-    day16("./inputs/day16.txt".to_string(), true);
+    day17("./inputs/day17small.txt".to_string(), true);
+    day17("./inputs/day17.txt".to_string(), true);
 }
