568e8c0f332e549ad537baa69b9fe6155818511b
Elliot Smith <elliot@elliotcsmith.com>
2024-12-18 15:58:12 +1000

    day 18

--- src/done.rs
+++ src/done.rs
@@ -11,6 +11,152 @@ use std::{
 use regex::Regex;
 use tracing::{Level, debug, error, event, info, instrument, warn};
 
+#[instrument]
+pub fn day17(filename: String, part_b: bool) {
+    let content = fs::read_to_string(filename).expect("Couldn't read input");
+
+    let mut register_a: u64 = 0;
+    let mut register_b: u64 = 0;
+    let mut register_c: u64 = 0;
+    let mut instruction_pointer: usize = 0;
+    let mut instructions: Vec<u8> = Vec::new();
+
+    let mut target: String = "".to_string();
+
+    for (i, line) in content.lines().enumerate() {
+        if line.len() == 0 {
+            continue;
+        }
+
+        let (_, right) = line.split_once(": ").unwrap();
+
+        match i {
+            0 => {
+                register_a = right.parse().unwrap();
+            }
+            1 => {
+                register_b = right.parse().unwrap();
+            }
+            2 => {
+                register_c = right.parse().unwrap();
+            }
+            3 => {}
+            4 => {
+                instructions = right.split(',').map(|x| x.parse().unwrap()).collect();
+            }
+            _ => {}
+        }
+    }
+    debug!(a=register_a, b=register_b, c=register_c, instructions=?instructions, "Read Program");
+
+    let mut out_pointer;
+    let mut a_test = 0;
+
+    if part_b {
+        a_test = 0; //8u64.pow(instructions.len() as u32);
+    }
+
+    loop {
+        register_a = (a_test << 24) + 0b11101001011011010010011100000;
+        register_b = 0;
+        register_c = 0;
+        instruction_pointer = 0;
+
+        out_pointer = 0;
+        loop {
+            if instruction_pointer >= instructions.len() {
+                debug!("End of Sequence");
+                break;
+            }
+            let op_code = instructions[instruction_pointer];
+            let operand = instructions[instruction_pointer + 1];
+
+            let operand_value = match operand {
+                0 | 1 | 2 | 3 => operand as u64,
+                4 => register_a,
+                5 => register_b,
+                6 => register_c,
+                7 => 0,
+                _ => 0,
+            };
+
+            match op_code {
+                0 => {
+                    // adv
+                    let div = 2u64.pow(operand_value as u32) as u64;
+                    let result = register_a / div;
+                    register_a = result;
+                }
+                1 => {
+                    // bxl
+                    let result = register_b ^ operand as u64;
+                    register_b = result;
+                }
+                2 => {
+                    // bst
+                    let result = operand_value % 8;
+                    register_b = result;
+                }
+                3 => {
+                    // jnz
+                    if register_a != 0 {
+                        instruction_pointer = operand as usize;
+                        continue;
+                    }
+                }
+                4 => {
+                    // bxc
+                    register_b = register_b ^ register_c;
+                }
+                5 => {
+                    // out
+                    let result = operand_value % 8;
+                    if result as u8 != instructions[out_pointer as usize] {
+                        if out_pointer > 8 {
+                            debug!(
+                                a = a_test,
+                                out = out_pointer,
+                                "Bailed on output mismatch {:b}",
+                                a_test
+                            );
+                        }
+                        break;
+                    }
+
+                    out_pointer += 1;
+
+                    if out_pointer >= instructions.len() {
+                        debug!("Bailed on output Length");
+                    }
+                }
+                6 => {
+                    // bdv
+                    let div = 2u64.pow(operand_value as u32) as u64;
+                    let result = register_a / div;
+                    register_b = result;
+                }
+                7 => {
+                    // cdv
+                    let div = 2u64.pow(operand_value as u32) as u64;
+                    let result = register_a / div;
+                    register_c = result;
+                }
+                _ => {
+                    error!(op_code = op_code, "Invalid Op Code");
+                }
+            };
+            instruction_pointer += 2;
+        }
+        if out_pointer == instructions.len() {
+            register_a = (a_test << 24) + 0b11101001011011010010011100000;
+            info!(a_test = register_a, "Finished Run");
+            break;
+        }
+
+        a_test += 1;
+    }
+}
+
 fn display_map(walls: &HashSet<(i32, i32)>, spots: &HashSet<(i32, i32)>, rows: i32, cols: i32) {
     for row in 0..rows {
         for col in 0..cols {
@@ -935,7 +1081,7 @@ pub fn day11(filename: String) {
     info!("Final Stones Recursive {:?}", total);
 }
 
-const LURD: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, 1), (0, -1)];
+pub const LURD: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, 1), (0, -1)];
 
 struct Step {
     pos: (i32, i32),
--- src/main.rs
+++ src/main.rs
@@ -17,149 +17,90 @@ pub fn dayxx(filename: String, part_b: bool) {
     let content = fs::read_to_string(filename).expect("Couldn't read input");
 }
 
+fn display_18_map(map: &HashSet<(i32, i32)>, size: i32) {
+    for r in 0..=size {
+        for c in 0..=size {
+            if map.contains(&(r, c)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+}
+
 #[instrument]
-pub fn day17(filename: String, part_b: bool) {
+pub fn day18(filename: String, part_b: bool, size: i32, steps: usize, first_jump: i32) {
     let content = fs::read_to_string(filename).expect("Couldn't read input");
 
-    let mut register_a: u64 = 0;
-    let mut register_b: u64 = 0;
-    let mut register_c: u64 = 0;
-    let mut instruction_pointer: usize = 0;
-    let mut instructions: Vec<u8> = Vec::new();
+    let mut position: (i32, i32) = (0, 0);
+    let mut goal: (i32, i32) = (size, size);
 
-    let mut target: String = "".to_string();
+    let mut blockers: Vec<(i32, i32)> = Vec::new();
 
-    for (i, line) in content.lines().enumerate() {
-        if line.len() == 0 {
-            continue;
-        }
-
-        let (_, right) = line.split_once(": ").unwrap();
-
-        match i {
-            0 => {
-                register_a = right.parse().unwrap();
-            }
-            1 => {
-                register_b = right.parse().unwrap();
-            }
-            2 => {
-                register_c = right.parse().unwrap();
-            }
-            3 => {}
-            4 => {
-                instructions = right.split(',').map(|x| x.parse().unwrap()).collect();
-            }
-            _ => {}
-        }
-    }
-    debug!(a=register_a, b=register_b, c=register_c, instructions=?instructions, "Read Program");
-
-    let mut out_pointer;
-    let mut a_test = 0;
-
-    if part_b {
-        a_test = 0; //8u64.pow(instructions.len() as u32);
+    for (r, line) in content.lines().enumerate() {
+        let (col, row) = line.split_once(',').unwrap();
+        blockers.push((row.parse::<i32>().unwrap(), col.parse::<i32>().unwrap()));
     }
 
+    let mut steps = steps;
+    let mut jump = first_jump;
     loop {
-        register_a = (a_test << 24) + 0b11101001011011010010011100000;
-        register_b = 0;
-        register_c = 0;
-        instruction_pointer = 0;
+        // Path find on a changing map. Could work with an exhaustive search.
+        let mut front: Vec<((i32, i32), i32)> = Vec::new();
+        let mut scores: HashMap<(i32, i32), i32> = HashMap::new();
+        front.push((position, 0)); // Store position + time
+        scores.insert(position, 0);
 
-        out_pointer = 0;
-        loop {
-            if instruction_pointer >= instructions.len() {
-                debug!("End of Sequence");
+        // Simulate X steps
+        let mut map: HashSet<(i32, i32)> = HashSet::new();
+        for t in 0..steps {
+            map.insert(blockers[t]);
+        }
+
+        // display_18_map(&map, size);
+
+        while let Some((pos, distance)) = front.pop() {
+            let mut early = false;
+            for dir in [
+                Direction::North,
+                Direction::South,
+                Direction::East,
+                Direction::West,
+            ] {
+                let test = move_direction(&pos, &dir);
+                if is_in_bounds(size + 1, size + 1, test.0, test.1) && !map.contains(&test) {
+                    let current = scores.get(&test).unwrap_or(&(distance + 2)).clone();
+                    if distance + 1 < current {
+                        front.push((test, distance + 1));
+                        scores.insert(test, distance + 1);
+                        if test == goal {
+                            early = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if early {
                 break;
             }
-            let op_code = instructions[instruction_pointer];
-            let operand = instructions[instruction_pointer + 1];
-
-            let operand_value = match operand {
-                0 | 1 | 2 | 3 => operand as u64,
-                4 => register_a,
-                5 => register_b,
-                6 => register_c,
-                7 => 0,
-                _ => 0,
-            };
-
-            match op_code {
-                0 => {
-                    // adv
-                    let div = 2u64.pow(operand_value as u32) as u64;
-                    let result = register_a / div;
-                    register_a = result;
-                }
-                1 => {
-                    // bxl
-                    let result = register_b ^ operand as u64;
-                    register_b = result;
-                }
-                2 => {
-                    // bst
-                    let result = operand_value % 8;
-                    register_b = result;
-                }
-                3 => {
-                    // jnz
-                    if register_a != 0 {
-                        instruction_pointer = operand as usize;
-                        continue;
-                    }
-                }
-                4 => {
-                    // bxc
-                    register_b = register_b ^ register_c;
-                }
-                5 => {
-                    // out
-                    let result = operand_value % 8;
-                    if result as u8 != instructions[out_pointer as usize] {
-                        if out_pointer > 8 {
-                            debug!(
-                                a = a_test,
-                                out = out_pointer,
-                                "Bailed on output mismatch {:b}",
-                                a_test
-                            );
-                        }
-                        break;
-                    }
-
-                    out_pointer += 1;
-
-                    if out_pointer >= instructions.len() {
-                        debug!("Bailed on output Length");
-                    }
-                }
-                6 => {
-                    // bdv
-                    let div = 2u64.pow(operand_value as u32) as u64;
-                    let result = register_a / div;
-                    register_b = result;
-                }
-                7 => {
-                    // cdv
-                    let div = 2u64.pow(operand_value as u32) as u64;
-                    let result = register_a / div;
-                    register_c = result;
-                }
-                _ => {
-                    error!(op_code = op_code, "Invalid Op Code");
-                }
-            };
-            instruction_pointer += 2;
-        }
-        if out_pointer == instructions.len() {
-            register_a = (a_test << 24) + 0b11101001011011010010011100000;
-            info!(a_test = register_a, "Finished Run");
-            break;
+            front.sort_by_key(|x| x.1);
         }
 
-        a_test += 1;
+        if let None = scores.get(&goal) {
+            if jump > 1 {
+                debug!(steps = jump, "little jump");
+                steps -= 2 * jump as usize;
+                jump = 1;
+            } else {
+                info!(distance = steps, blocker = ?blockers[steps-1], "Finished");
+                break;
+            }
+        } else {
+            debug!(steps = steps, "Continuing");
+            steps += jump as usize;
+        }
     }
 }
 
@@ -169,6 +110,6 @@ fn main() {
         .init();
     info!("Tracing Setup");
 
-    //day17("./inputs/day17bsmall.txt".to_string(), true);
-    day17("./inputs/day17.txt".to_string(), true);
+    day18("./inputs/day18small.txt".to_string(), false, 6, 12, 1);
+    day18("./inputs/day18.txt".to_string(), false, 70, 1024, 100);
 }
